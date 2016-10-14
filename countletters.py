4a640b4fcee648beb86b9910e0f8bfbe0eadfdc1
Daniela Güemez <danielaguemez@gmail.com>
2016-10-13 23:01:12 -0500

    Add files via upload

--- countletters.py
+++ countletters.py
@@ -1,13 +1,17 @@
-user=str(input("Type a string: "))
-new_user=user.lower()
-times=new_user.count("a")
-letter=str(input("What letter do you want me to count? ")).lower()
+abecedario=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r","s","t","u", "v", "w", "x", "y", "z"]
 
-def letters(letter):
-    times=new_user.count(letter)
-    return times
+def letters(text):
+    letras={}#we create a dictionary to store the letter with is't respective value
+    new_text=text.lower()#we make the user input lowercase
+    for letter in abecedario:
+        times=new_text.count(letter)#the function counts whatever char you specify
+        if times==0:#if a letter is not in the text we do not put it in the dictionary
+            continue#we return to the for
+        else:
+            letras[letter]=times#we append to the dictionary the letter with the value of times that it repeated
+    return letras
 
-
-answer=letters(letter)
-
-print ("The letter "+str(letter)+" is "+str(times)+" times in the sentence "+str(new_user))
+print ("We will count how many times you repeat each letter in a text")
+user=str(input("Type a string: "))#we ask the user for a string to count the letters of
+answer=letters(user)#we run the function
+print (answer)
