67b4bd9b9c91409a14058171e0d017d157c1ac3c
Daniela Güemez <danielaguemez@gmail.com>
2016-10-13 22:38:06 -0500

    Add files via upload

--- palindromes.py
+++ palindromes.py
@@ -1,10 +1,18 @@
 #palindromes
+
+def filtrar(text):
+    not_acceptable_but_with_space=[",",";",".","$","#","%","&","(",")","?",":","@","!"]
+    filtered_words=text
+    for i in not_acceptable_but_with_space:
+        filtered_words=filtered_words.split(i)
+        filtered_words="".join(filtered_words)
+    return filtered_words
+
+
 print("Hey, let's see if what you type is a palindrome")
 user_input=str(input()).lower()
 not_acceptable_characters=[",",";",".","$","#","%","&","(",")","?"," ",":","@","!"]
-not_acceptable_but_with_space=[",",";",".","$","#","%","&","(",")","?",":","@","!"]
 filtered_input=user_input
-filtered_words=user_input
 for char in not_acceptable_characters:
     filtered_input=filtered_input.split(char)
     filtered_input="".join(filtered_input)
@@ -15,11 +23,9 @@ if reversed_input==filtered_input:
 else:
     print("The sentence  "+filtered_input+" is not a palindrome")
 
-for i in not_acceptable_but_with_space:
-    filtered_words=filtered_words.split(i)
-    filtered_words="".join(filtered_words)
+filtered=filtrar(user_input)
 
-filtered_list=filtered_words.split()
+filtered_list=filtered.split()
 
 for word in filtered_list:
     if word==word[::-1]:
